f0854384988565d39ae8c1f99b7720e383a2f285
Fred <guru.three@gmail.com>
2013-05-01 13:48:21 +0100

    Script for sending SMS messages from the command line interface via the BulkSMS service.

---
+++ bin/sendbulksms.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# check to makre sure we’ve got the right number of arguments
+if [[ $# -ne 2 ]]
+then
+	echo "$0 \"message\" phonenumber"
+	echo "Phone number should have country code, no +"
+	echo "Make sure this script has been edited for the correct BULKSMS username and password."
+	exit 1
+fi
+
+# set character encoding, unknown if this is necessary
+export LANG=iso8859-1
+export LC_ALL=iso8859-1
+
+# details
+USERNAME="username"
+PASSWORD="password"
+MESSAGE="$1"
+PHONE="$2"
+DUPID=$[`date +%s` * $RANDOM]
+DUPID=`echo $DUPID | md5sum | awk '{print $1}'`
+
+# send the message
+curl http://www.bulksms.co.uk/eapi/submission/send_sms/2/2.0 --data-urlencode stop_dup_id=$DUPID --data-urlencode username="$USERNAME" --data-urlencode password="$PASSWORD" --data-urlencode message="$MESSAGE" --data-urlencode msisdn="$PHONE"
+
