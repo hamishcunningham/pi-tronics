38d786a5d83dfbffa1d4a5e1c08c6b5c79799ded
Hamish Cunningham <hamish@gate.ac.uk>
2013-08-09 17:52:04 +0300

    debugging and test scripts

--- simbamon/utils/monitor-log.sh
+++ simbamon/utils/monitor-log.sh
@@ -1,1 +1,9 @@
-while :; do clear; grep simbamon /var/log/syslog |tail -20; sleep 2; done
+#!/usr/bin/env bash
+# monitor-log.sh -- grep simbamon syslog entries
+
+while :
+do
+  clear
+  grep simbamon /var/log/syslog |tail -20
+  sleep 2
+done
--- simbamon/utils/set-simulation-level.sh
+++ simbamon/utils/set-simulation-level.sh
@@ -1,1 +1,13 @@
-while :; do echo -n 'level? '; read LEVEL; clear; echo 100 >/tmp/simbamon-simulation.txt; cat /tmp/simbamon-simulation.txt; done
+#!/usr/bin/env bash
+# set-simulation-level.sh -- read and write battery level simulation values
+
+while :
+do
+  echo -n 'level? '
+  read LEVEL
+  clear
+  echo ${LEVEL} >/tmp/simbamon-simulation.txt
+  echo -n 'level set to: '
+  cat /tmp/simbamon-simulation.txt
+  echo
+done
