7dfe590a1b4ec36497af5ba58c82d24456c78758
Hamish Cunningham <hamish@gate.ac.uk>
2013-08-11 18:15:57 +0300

    first cut

--- blinkip/blink_ip.pl
+++ blinkip/blink_ip.pl
@@ -1,13 +1,21 @@
 #!/usr/bin/perl
+#
+# This code was derived from Chris Meyers' script described here --
+# http://chrismeyers.org/2012/06/02/raspberry-pi-no-hdmi-cable-or-no-hdmi-display-blink-ip-address-to-ok-led/
+# -- and available here -- https://gist.github.com/chrismeyersfsu/2858824
+
 my $dev = "/sys/class/leds/led0/brightness";
 my $OFF = "1";
 my $ON = "0";
 
 my ($ip) = `ifconfig`=~/ddr:(.*?) /;
-#print $ip;
+print "$ip\n";
 
 # Strip out the .'s
 $ip =~ s/\.//g;
+print "$ip\n";
+
+exit;
 
 while (1) {
     display_start();
--- blinkip/utils/monitor-log.sh
+++ blinkip/utils/monitor-log.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# monitor-log.sh -- grep simbamon syslog entries
+# monitor-log.sh -- grep blinkip syslog entries
 
 while :
 do
   clear
-  grep simbamon /var/log/syslog |tail -20
+  grep blinkip /var/log/syslog |tail -20
   sleep 2
 done
