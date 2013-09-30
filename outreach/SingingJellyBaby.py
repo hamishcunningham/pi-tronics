07458f673b5ba1a6a1e40e89090535e792f9fab8
Hamish Cunningham <hamish@gate.ac.uk>
2013-09-30 12:15:01 +0000

    alternative sound file

--- outreach/SingingJellyBaby.py
+++ outreach/SingingJellyBaby.py
@@ -11,7 +11,8 @@ GPIO.setup(3, GPIO.IN)
 while True:
   if GPIO.input(3) == False:
     print("time to sing!")
-    os.system('mpg321 la.mp3 &')
+    # os.system('mpg321 la.mp3 &')
+    os.system('aplay police_s.wav &')
 
   print("time to sleep...")
   time.sleep(2)
