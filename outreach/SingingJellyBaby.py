be4bb2443b0a3a5e1e045d121055602f9614b8c1
Hamish Cunningham <hamish@gate.ac.uk>
2013-09-30 12:04:18 +0000

    working version of singing jelly baby

--- outreach/SingingJellyBaby.py
+++ outreach/SingingJellyBaby.py
@@ -5,10 +5,13 @@ import RPi.GPIO as GPIO
 import os
 
 GPIO.cleanup()
-GPIO.semode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.IN)
 
 while True:
   if GPIO.input(3) == False:
+    print("time to sing!")
     os.system('mpg321 la.mp3 &')
-  time.sleep(1)
+
+  print("time to sleep...")
+  time.sleep(2)
