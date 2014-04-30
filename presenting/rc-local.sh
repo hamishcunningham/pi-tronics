41b4f639a0f70d1effd2a318d2c1b026f73de224
Hamish Cunningham <hamish@gate.ac.uk>
2014-04-30 15:25:37 +0100

    more options for presentation mode

--- presenting/rc-local.sh
+++ presenting/rc-local.sh
@@ -1,4 +1,12 @@
 
-# presentation mode: uncomment the below
+# presentation mode: uncomment ONE OF the below
+#
+#   either:
 # /usr/bin/xinit /usr/bin/xpdf -fullscreen /home/pi/talk.pdf
+#
+#   or:
+# sudo -u pi /usr/bin/startx &
+#   in the latter case also add ~./.xinitrc containing
+#   /usr/bin/evince -s /home/pi/talk.pdf &
+#   /usr/bin/x-window-manager
 
