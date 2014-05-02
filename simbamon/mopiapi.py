8bbf3ce0057a270c1e23d9b600689397a68661e1
Hamish Cunningham <hamish@gate.ac.uk>
2014-05-02 11:46:06 +0100

    partially updated for 3.03 firmware (from fred)

--- simbamon/mopiapi.py
+++ simbamon/mopiapi.py
@@ -1,7 +1,7 @@
 import smbus
 
-# version 0.1
-# for mopi firmware v3.02
+# version 0.2
+# for mopi firmware v3.03
 
 class mopiapi():
 	device = 0xb
@@ -10,34 +10,43 @@ class mopiapi():
 		self.bus = smbus.SMBus(i2cbus)
 
 	def getStatus(self):
-		return self.bus.read_byte_data(self.device, 0b00000000)
+		return self.bus.read_word_data(self.device, 0b00000000)
 
-	def getVoltage(self):
-		return self.bus.read_word_data(self.device, 0b00000001)
+	def getVoltage(self, input=0):
+		if input == 1:
+			return self.bus.read_word_data(self.device, 0b00000101) # 5
+		elif input == 2:
+			return self.bus.read_word_data(self.device, 0b00000110) # 6
+		else:
+			return self.bus.read_word_data(self.device, 0b00000001)
 
-	# returns an array of 6 integers: max1, mid1, min1, max2, mid2, min2 (mV)
-	def readConfig(self):
-		data = self.bus.read_i2c_block_data(self.device, 0b00000010)
+	# returns an array of 3 integers: max, mid, min (mV)
+	def readConfig(self, input=0):
+		if input == 1:
+			data = self.bus.read_i2c_block_data(self.device, 0b00000111) # 7
+		elif input == 2:
+			data = self.bus.read_i2c_block_data(self.device, 0b00001000) # 8
+		else:
+			data = self.bus.read_i2c_block_data(self.device, 0b00000010)
 		data2 = []
-		data2.append((data[1] << 8) + data[0])
-		data2.append((data[3] << 8) + data[2])
-		data2.append((data[5] << 8) + data[4])
-		data2.append((data[7] << 8) + data[6])
-		data2.append((data[9] << 8) + data[8])
-		data2.append((data[11] << 8) + data[10])
+		data2.append((data[0] << 8) + data[1])
+		data2.append((data[2] << 8) + data[3])
+		data2.append((data[4] << 8) + data[5])
 		return data2
 
-	# takes an array of 6 integers: max1, mid1, min1, max2, mid2, min2 (mV)
-	def writeConfig(self, battery):
+	# takes an array of 6 integers: max, mid, min (mV)
+	def writeConfig(self, battery, input=0):
 		data = [
-			battery[0] & 0xff, battery[0] >> 8, \
-			battery[1] & 0xff, battery[1] >> 8, \
-			battery[2] & 0xff, battery[2] >> 8, \
-			battery[3] & 0xff, battery[3] >> 8, \
-			battery[4] & 0xff, battery[4] >> 8, \
-			battery[5] & 0xff, battery[5] >> 8, \
+			battery[0] >> 8, battery[0] & 0xff, \
+			battery[1] >> 8, battery[1] & 0xff, \
+			battery[2] >> 8, battery[2] & 0xff, \
 			]
-		self.bus.write_i2c_block_data(self.device, 0b00000010, data)
+		if input == 1:
+			self.bus.write_i2c_block_data(self.device, 0b00000111, data) # 7
+		elif input == 2:
+			self.bus.write_i2c_block_data(self.device, 0b00001000, data) # 8
+		else:
+			self.bus.write_i2c_block_data(self.device, 0b00000010, data)
 
 	def setPowerOnDelay(self, poweron):
 		self.bus.write_word_data(self.device, 0b00000011, poweron)
@@ -51,20 +60,34 @@ class mopiapi():
 	def getShutdownDelay(self):
 		return self.bus.read_word_data(self.device, 0b00000100)
 
+	def getFirmwareVersion(self):
+		return self.bus.read_word_data(self.device, 0b00000101) # 9
+
+	def getSerialNumber(self):
+		return self.bus.read_word_data(self.device, 0b00000110) # 10
+
 def statusDetail(byte):
 	out = ""
-	if (byte >> 7):
-		out += 'Shutdown delay has been set\n'
-	if (byte & 0b1000000) >> 6:
-		out += 'Power on delay has been set\n'
+	if (byte & 0b100000000000) >> 10:
+		out += 'Shutdown delay in progress\n'
+	if (byte & 0b10000000000) >> 10:
+		out += 'Shutdown delay set\n'
+	if (byte & 0b1000000000) >> 9:
+		out += 'Power on delay in progress\n'
+	if (byte & 0b100000000) >> 8:
+		out += 'Power on delay set\n'
+	if (byte & 0b10000000) >> 7:
+		out += 'Forced shutdown\n'
+#	if (byte & 0b1000000) >> 6:
+#		out += 'Unused!\n'
 	if (byte & 0b100000) >> 5:
-		out += 'UPS mode\n'
+		out += 'Battery critical (flashing red led)\n'
 	if (byte & 0b10000) >> 4:
-		out += 'Forced shutdown (button pressed)\n'
+		out += 'Battery low (red led)\n'
 	if (byte & 0b1000) >> 3:
-		out += 'Battery critical\n'
+		out += 'Battery good (green led)\n'
 	if (byte & 0b100) >> 2:
-		out += 'Battery low\n'
+		out += 'Battery full (blue led)\n'
 	if (byte & 0b10) >> 1:
 		out += 'Battery #2 active\n'
 	if byte & 1:
