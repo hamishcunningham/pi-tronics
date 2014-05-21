4707013257f1c650fb77d706af5973dd3e26195e
Hamish Cunningham <hamish@gate.ac.uk>
2014-05-21 11:59:11 +0100

    add date to status log

--- simbamon/utils/monitor-status.sh
+++ simbamon/utils/monitor-status.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # monitor-status -- loop reading status
 
+do_date() {
+  date "+%Y-%m-%d-%T" |tr '[A-Z]' '[a-z]' |sed 's,:,,g'
+}
 do_stat() {
   sudo mopicli -e | pr -t3 -w100
 }
 
-NOW=`date "+%Y-%m-%d-%T" |tr '[A-Z]' '[a-z]' |sed 's,:,,g'`
+NOW=`do_date`
 
 clear
 while :; do
+  echo "MoPi status at `do_date`:"
   do_stat
   echo
   sleep 3
