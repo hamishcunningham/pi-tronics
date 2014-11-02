a37d632852c82c99c81bca7d5f7ac65f141db041
Fred <guru.three@gmail.com>
2014-11-02 10:46:51 +0000

    Better HTML

--- sts-robot/remote-control.py
+++ sts-robot/remote-control.py
@@ -9,74 +9,79 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
 #        s.wfile.write("test")
 #        print s.path
-        if s.path == "/test.jpg":
-            f = open('test.jpg', 'rb')
+
+        if s.path != "/":
+            if s.path == "/test.jpg":
+                f = open('test.jpg', 'rb')
+                s.send_response(200)
+                s.send_header("Content-type", "image/jpg")
+                s.end_headers()
+                s.wfile.write(f.read())
+                return
+            elif s.path == "/test.html":
+                s.send_response(200)
+                s.end_headers()
+                s.wfile.write('<html><head><meta http-equiv="refresh" content="2"></head><body><p align="center"><img src="/test.jpg" width="640" height="480"></p></body></html>')
+                return
+
+            # forward for a short period
+            elif s.path == "/forward":
+                pibrella.output.on()
+                time.sleep(0.5)
+                pibrella.output.off()
+
+            # turn for a short period
+            elif s.path == "/left":
+                pibrella.output.f.on()
+                time.sleep(0.5)
+                pibrella.output.f.off()
+            elif s.path == "/right":
+                pibrella.output.e.on()
+                time.sleep(0.5)
+                pibrella.output.e.off()
+
+            # forward indefinetely
+            elif s.path == "/forwardstart":
+                pibrella.output.on()
+            elif s.path == "/forwardstop":
+                pibrella.output.off()
+
+            # turn indefinetely
+            elif s.path == "/leftstart":
+                pibrella.output.f.on()
+            elif s.path == "/rightstart":
+                pibrella.output.e.on()
+            elif s.path == "/leftstop":
+                pibrella.output.f.off()
+            elif s.path == "/rightstop":
+                pibrella.output.e.off()
             s.send_response(200)
-            s.send_header("Content-type", "image/jpg")
             s.end_headers()
-            s.wfile.write(f.read())
-            return
-        if s.path == "/test.html":
-            s.send_response(200)
-            s.end_headers()
-            s.wfile.write('<html><head><meta http-equiv=\"refresh"\ content=\"2\"></head><body><p align=\"center\"><img src="/test.jpg" width=\"640\" height=\"480\"></p></body></html>')
+            s.wfile.write("done");
             return
 
-        # forward for a short period
-        if s.path == "/forward":
-            pibrella.output.on()
-            time.sleep(0.5)
-            pibrella.output.off()
-
-        # turn for a short period
-        if s.path == "/left":
-            pibrella.output.f.on()
-            time.sleep(0.5)
-            pibrella.output.f.off()
-        if s.path == "/right":
-            pibrella.output.e.on()
-            time.sleep(0.5)
-            pibrella.output.e.off()
-
-        # forward indefinetely
-        if s.path == "/forwardstart":
-            pibrella.output.on()
-        if s.path == "/forwardstop":
-            pibrella.output.off()
-
-        # turn indefinetely
-        if s.path == "/leftstart":
-            pibrella.output.f.on()
-        if s.path == "/rightstart":
-            pibrella.output.e.on()
-        if s.path == "/leftstop":
-            pibrella.output.f.off()
-        if s.path == "/rightstop":
-            pibrella.output.e.off()
-
-#        if s.path == "/":
+#       otherwise s.path == "/"...
         s.send_response(200)
         s.end_headers()
-        s.wfile.write("<head><style>a { font-size: large; color: black; border: 3px solid black; padding: 20px; } p { padding: 3px }</style></head>")
-        s.wfile.write("<body><h1 align=\"center\">A simple, open robot</h1><p><br/>")
-#        s.wfile.write("<body style=\"font-size: 200%\">")
-        s.wfile.write("<p align=\"center\"><a href=\"/forward\">forward</a> ")
-        s.wfile.write("<a href=\"/left\">left</a> ")
-        s.wfile.write("<a href=\"/right\">right</a></p>")
-        s.wfile.write("<p align=\"center\">&nbsp;</p>")
-        s.wfile.write("<p align=\"center\"><a href=\"/forwardstart\">forwardstart</a> ")
-        s.wfile.write("<a href=\"/forwardstop\">forwardstop</a></p>")
+        s.wfile.write('<html><head><style>a { font-size: large; color: black; border: 3px solid black; padding: 1em; } p { padding: 3px; margin-top: 2.5em; }</style></head>\n')
+        s.wfile.write('<body><h1 align="center">A simple, open robot</h1>\n')
 
-        s.wfile.write("<p align=\"center\">&nbsp;</p>")
-        s.wfile.write("<p align=\"center\"><a href=\"/leftstart\">leftstart</a> ")
-        s.wfile.write("<a href=\"/leftstop\">leftstop</a></p>")
+        s.wfile.write('<p align="center"><a href="/forward" target="myframe">forward</a> ')
+        s.wfile.write('<a href="/left" target="myframe">left</a> ')
+        s.wfile.write('<a href="/right" target="myframe">right</a></p>\n')
 
-        s.wfile.write("<p align=\"center\">&nbsp;</p>")
-        s.wfile.write("<p align=\"center\"><a href=\"/rightstart\">rightstart</a> ")
-        s.wfile.write("<a href=\"/rightstop\">rightstop</a></p>")
+        s.wfile.write('<p align="center"><a href="/forwardstart" target="myframe">forwardstart</a> ')
+        s.wfile.write('<a href="/forwardstop" target="myframe">forwardstop</a></p>\n')
 
-#        s.wfile.write("<p><br/><p align=\"center\"><iframe src=\"/test.html\" width=\"680\" height=\"520\"></iframe></p>")
-        s.wfile.write("</body>")
+        s.wfile.write('<p align="center"><a href="/leftstart" target="myframe">leftstart</a> ')
+        s.wfile.write('<a href="/leftstop" target="myframe">leftstop</a></p>\n')
+
+        s.wfile.write('<p align="center"><a href="/rightstart" target="myframe">rightstart</a> ')
+        s.wfile.write('<a href="/rightstop" target="myframe">rightstop</a></p>\n')
+
+#        s.wfile.write('<p><br/><p align="center"><iframe src="/test.html" width="680" height="520"></iframe></p>')
+        s.wfile.write('<iframe src="/" name="myframe" style="display: none">\n')
+        s.wfile.write('</body></html>\n')
 
 
 server_class=BaseHTTPServer.HTTPServer
