1ca913b3334e9397fb77a8dfdcfec7918c76e914
Hamish Cunningham <hamish@gate.ac.uk>
2015-07-03 11:59:06 +0100

    started adding video creation code

--- fishcam/timelapse.sh
+++ fishcam/timelapse.sh
@@ -41,5 +41,40 @@ picsloop() {
   done
 }
 [ -d $PICSDIR ] || mkdir -p $PICSDIR
+
+# create a vid
+makevid() {
+  # from http://www.raspberrypi-spy.co.uk/2013/05/creating-timelapse-videos-with-the-raspberry-pi-camera/
+  avconv \
+    -r 10 -i timelapse_%04d.jpg \
+    -r 10 -vcodec libx264 -crf 20 -g 15 \
+    -vf crop=2592:1458,scale=1280:720 \
+  timelapse.mp4
+  return 0
+
+  # alternatives:
+
+  # from http://www.instructables.com/id/Simple-timelapse-camera-using-Raspberry-Pi-and-a-c/?ALLSTEPS
+  mencoder -nosound -ovc lavc -lavcopts \
+    vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 \
+    -o timelapse.avi -mf type=jpeg:fps=24 mf://@list.txt
+
+  # from https://www.raspberrypi.org/learning/timelapse-setup/worksheet.md
+  mencoder -nosound -ovc lavc -lavcopts \
+    vcodec=mpeg4:aspect=16/9:vbitrate=8000000 \
+    -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=24 mf://@stills.txt
+
+  # from http://blog.davidsingleton.org/raspberry-pi-timelapse-controller/
+  # (also discusses exposure times and flicker at sunset)
+  ffmpeg -r 18 -q:v 2 -start_number XXXX -i /tmp/timelapse/IMG_%d.JPG \
+    output.mp4
+
+  # from http://computers.tutsplus.com/tutorials/creating-time-lapse-photography-with-a-raspberry-pi--cms-20794
+  mencoder -nosound -ovc lavc -lavcopts \
+    vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 \
+    -o timelapse.avi -mf type=jpeg:fps=24 mf://@stills.txt
+}
+
+# do summut
 cd $PICSDIR
 picsloop
