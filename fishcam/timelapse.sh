a60d45f2a862f3d80561a5cae8bc3c249cfb95a1
Hamish Cunningham <hamish@gate.ac.uk>
2015-07-07 09:55:15 +0100

    thumb server working

--- fishcam/timelapse.sh
+++ fishcam/timelapse.sh
@@ -123,15 +123,14 @@ picsloop() {
 
 # serve the thumbnails
 servehttp() {
-  sudo su pi -c \
-   "cd /home/pi/fishpics &&
-    echo '<ul>'   >index.html && \
-    for d in 20*; \
-    do \
-      echo '<li><a href='${d}'/>'${d}'/</a></li>'; \
-    done          >>index.html && \
-    echo '</ul>'  >>index.html"
-  cd /home/pi/fishpics && python -m SimpleHTTPServer
+  cd /home/pi/fishpics
+  echo '<ul>'   >index.html
+  for d in `find . -type d`
+  do
+    echo '<li><a href='${d}'/>'${d}'/</a></li>'
+  done          >>index.html
+  echo '</ul>'  >>index.html
+  python -m SimpleHTTPServer
 }
 
 # halt cameras
